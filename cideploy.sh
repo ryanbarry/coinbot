84fdbac35fb93b192c732936f7fba8d079c819de
Ryan Barry <ryan@nuclearice.com>
2017-08-27 02:21:09 -0700

    match image tags used for push & run

--- cideploy.sh
+++ cideploy.sh
@@ -7,5 +7,5 @@ echo $KEY_PEM | sed 's/\\n/\n/g' > key.pem
 cd
 docker rm -f coinbot
 docker ps -a
-docker run --label com.joyent.package=g4-highcpu-128M -d --name=coinbot -e SLACK_TOKEN=$SLACK_TOKEN ryanbarry/coinbot:$CI_BUILD_ID
+docker run --label com.joyent.package=g4-highcpu-128M -d --name=coinbot -e SLACK_TOKEN=$SLACK_TOKEN ryanbarry/coinbot:$CI_COMMIT_ID
 docker ps -a
